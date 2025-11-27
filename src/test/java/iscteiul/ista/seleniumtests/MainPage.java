39b38f9dca555b6b3f1531a468f9d3c3d2af1a2d
LEI-123302 <bfdno@iscte-iul.pt>
2025-11-27 10:49:08 +0000

    Todos os testes executados com sucesso

--- src/test/java/iscteiul/ista/seleniumtests/MainPage.java
+++ src/test/java/iscteiul/ista/seleniumtests/MainPage.java
@@ -4,9 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 // page_url = https://www.jetbrains.com/
 public class MainPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(xpath = "//button[contains(text(), 'Accept') or contains(text(), 'Accept All')]")
+    public WebElement acceptCookiesButton;
+
     @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
     public WebElement seeDeveloperToolsButton;
 
@@ -20,6 +29,20 @@ public class MainPage {
     public WebElement searchButton;
 
     public MainPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         PageFactory.initElements(driver, this);
+    }
+
+    public void acceptCookies() {
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(acceptCookiesButton)).click();
+            wait.until(ExpectedConditions.invisibilityOf(acceptCookiesButton));
+
+            System.out.println("Banner de cookies aceite e removido com sucesso.");
+
+        } catch (Exception e) {
+            System.out.println("Erro ao interagir com o banner de cookies: " + e.getMessage());
+        }
     }
 }
