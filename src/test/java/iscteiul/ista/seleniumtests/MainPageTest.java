c594c54c63b8321ca6a264d4301bebf5deb839db
LEI-123302 <bfdno@iscte-iul.pt>
2025-11-27 11:40:16 +0000

    Teste de interação complexa FileUpload

--- src/test/java/iscteiul/ista/seleniumtests/MainPageTest.java
+++ src/test/java/iscteiul/ista/seleniumtests/MainPageTest.java
@@ -1,31 +1,28 @@
 package iscteiul.ista.seleniumtests;
 
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.time.Duration;
 
 public class MainPageTest {
     private WebDriver driver;
     private MainPage mainPage;
-    private WebDriverWait wait;
 
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Espera explícita de 5s
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.jetbrains.com/");
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Espera explícita de 5s
-
         mainPage = new MainPage(driver);
-        mainPage.acceptCookies();
     }
 
     @AfterEach
@@ -35,46 +32,32 @@ public class MainPageTest {
 
     @Test
     public void search() {
-        wait.until(ExpectedConditions.elementToBeClickable(mainPage.searchButton));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", mainPage.searchButton);
         mainPage.searchButton.click();
 
-        WebElement searchField = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[data-test$='inner']"))
-        );
-
+        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
         searchField.sendKeys("Selenium");
-        assertEquals("Selenium", searchField.getAttribute("value"));
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
+        submitButton.click();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
+        assertEquals("Selenium", searchPageField.getAttribute("value"));
     }
 
     @Test
     public void toolsMenu() {
         mainPage.toolsMenu.click();
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        assertTrue(mainPage.findYourToolsButton.isDisplayed());
+        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
+        assertTrue(menuPopup.isDisplayed());
     }
 
     @Test
     public void navigationToAllTools() {
         mainPage.seeDeveloperToolsButton.click();
+        mainPage.findYourToolsButton.click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(mainPage.findYourToolsButton));
-
-        try {
-            mainPage.findYourToolsButton.click();
-        } catch (ElementClickInterceptedException e) {
-            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", mainPage.findYourToolsButton);
-        }
-
-        WebElement productsList = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.id("products-page"))
-        );
+        WebElement productsList = driver.findElement(By.id("products-page"));
         assertTrue(productsList.isDisplayed());
         assertEquals("All Developer Tools and Products by JetBrains", driver.getTitle());
     }
